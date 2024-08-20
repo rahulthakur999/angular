62530627ce20ec5ba702d60f56028a4b6ada8c63
Rahul Kumar <rahul.t@bettertechsolution.com>
2024-08-20 21:40:00 +0530

    UI

--- src/app/javascript/javascript/javascript-routing.module.ts
+++ src/app/javascript/javascript/javascript-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Lession1Component } from '../lession1/lession1.component';
+import { Lession2Component } from '../lession2/lession2.component';
 
 const routes: Routes = [
   {
     path: '',
     component: Lession1Component
+  },
+  {
+    path: 'lession1',
+    component: Lession1Component
+  }
+  ,
+  {
+    path: 'lession2',
+    component: Lession2Component
   }
 ];
 
